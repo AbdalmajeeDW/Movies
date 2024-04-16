dac6bdbe2a11131cee786fc9fe5be49f117f9c45
AbdalmajeeDW <abd.almajed.alhashemi@gmail.com>
2024-04-16 17:20:14 +0300

    fix import file

--- pages/moviesTrends/trends.vue
+++ pages/moviesTrends/trends.vue
@@ -1,13 +1,13 @@
-<script>
-import TrendMovies from "~/components/pages/Trend/trends.vue";
-export default {
-  components: {
-    TrendMovies: TrendMovies,
-  },
-};
-</script>
 <template >
   <div>
     <TrendMovies />
   </div>
 </template>
+  <script>
+  import TrendMovies from "../../components/pages/Trend/Trends.vue";
+  export default {
+    components: {
+      TrendMovies: TrendMovies,
+    },
+  };
+  </script>
