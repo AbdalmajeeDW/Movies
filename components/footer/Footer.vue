91341954845fb4e7d4f2e1b549ae08d37131ef42
AbdalmajeeDW <abd.almajed.alhashemi@gmail.com>
2024-04-20 00:57:02 +0300

    fix footer

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,24 +1,21 @@
 <template lang="">
-       <footer class="site-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-sm-6 col-xs-12">
+       <div class="site-footer">
+      
+          <div>
             <p class="copyright-text">Copyright &copy; 2024 All Rights Reserved by 
-         <a href="#">TMDB</a>.
+         <a href="#">TMDB</a>
             </p>
           </div>
-          <div class="col-md-4 col-sm-6 col-xs-12">
+          <div >
             <ul class="social-icons">
               <li><a class="facebook" href="#"><span class="mdi mdi-facebook"></span></i></a></li>
-              <li><a class="twitter" href="#">X</span>
-</a></li>
+              <li><a class="twitter" href="#">X</a></li>
               <li><a class="dribbble" href="#"><span class="mdi mdi-whatsapp"></span></a></li>
               <li><a class="linkedin" href="#"><span class="mdi mdi-linkedin"></span></a></li>   
             </ul>
           </div>
-        </div>
-      </div>
-</footer>
+      
+</div>
 </template>
 <script>
 export default {
